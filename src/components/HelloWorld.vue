18dae48f494dd2388075025373aca2742bba34a6
nazimjrf <nazim@jrfintel.com>
2025-04-21 17:12:50 +0600

    updated

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+      <h1>Hello World</h1>
+</template>
+
+<style>
+h1{
+      color: red;
+}
+</style>
+
+<script setup>
+alert("nazim")
+</script>
---
+++ src/components/MyEvent.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { reactive } from 'vue'
+let initState = reactive({ count: 0 })
+</script>
+
+<template>
+  <h1>Count: {{ initState.count }}</h1>
+  <button @click="initState.count++">+</button>
+  <button @click="initState.count--">-</button>
+</template>
+<style scoped></style>
