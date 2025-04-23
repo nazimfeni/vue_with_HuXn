7019010874bfab3e941e82f820ed0708234a5227
nazimjrf <nazim@jrfintel.com>
2025-04-23 11:01:04 +0600

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
+import CounterProgram from './components/CounterProgram.vue';
+
+const show = ref(true);
+
+function toggle (){
+  show.value = !show.value;
+}
 </script>
+
+
 <template>
-  <HelloWorld />
+  <div>
+    <h1>Vue Lifecycle Demo</h1>
+    <button @click="toggle">Toggle Component</button>
+
+    <!-- <CounterProgram v-if="show" /> -->
+    <keep-alive>
+      <CounterProgram v-if="show" />
+    </keep-alive>
+  
+  
+  </div>
 </template>
 
-<style scoped>
-</style>
+
---
+++ src/components/CounterProgram.vue
@@ -0,0 +1,71 @@
+<template>
+      <div>
+        <h2>Counter Component</h2>
+        <button @click="increment">Count is: {{ count }}</button>
+      </div>
+    </template>
+    
+    <script setup>
+    import {
+      ref,
+      onBeforeMount,
+      onMounted,
+      onBeforeUpdate,
+      onUpdated,
+      onBeforeUnmount,
+      onUnmounted,
+      onActivated,
+      onDeactivated,
+      onErrorCaptured
+    } from 'vue';
+    
+    const count = ref(100);
+    
+    function increment() {
+      count.value++;
+    }
+    
+    // Lifecycle Hooks of Vue
+    onBeforeMount(() => {
+      alert('Before Mount: Component is about to be added to the DOM');
+    });
+
+    onMounted(() => {
+      alert(`Mounted: The initial count is ${count.value}`);
+    });
+    onBeforeUpdate(() => {
+      alert('Before Update: DOM is about to change');
+    });
+    
+    onUpdated(() => {
+      alert('Updated: DOM changed because of reactive update');
+    });
+    
+    onBeforeUnmount(() => {
+      alert('Before Unmount: Component will be removed from DOM');
+    });
+    
+    onUnmounted(() => {
+      alert('Unmounted: Component removed from DOM');
+    });
+    
+    onActivated(() => {
+      alert('Activated: Component is now active (keep-alive)');
+    });
+    
+    onDeactivated(() => {
+      alert('Deactivated: Component is now inactive (keep-alive)');
+    });
+    
+    onErrorCaptured((err, instance, info) => {
+      alert('Error captured in component: ' + err.message);
+      return false; // prevent further propagation
+    });
+    </script>
+    
+    <style>
+    h1 {
+      color: teal;
+    }
+    </style>
+    
